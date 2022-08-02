04bd8d927a1a0c837f54e16f49b78790a9dd879d
vukanm <vukan.mirkovic@gmail.com>
2022-08-02 06:42:12 +0200

    Port change

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  await server.listen({port: process.env.PORT || 4000});
-
-  const { url, port } = await server.listen();
+  const { url, port } = await server.listen({port: process.env.PORT || 4000});
   console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
